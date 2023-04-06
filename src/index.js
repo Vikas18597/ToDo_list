f00b6d896ee026bda57aff24671daf04320d3bf4
Vikas18597 <khativikas97@gmail.com>
2023-04-07 01:32:25 +0530

    made add button with non null notes not being added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import './styles/main.css'
 
-const addnote = () => {
-    const list = document.createElement("li")
-    
-    document.querySelector(".tickets").appendChild(list);
-    const link = document.createElement("a")
-    list.appendChild(link)
+function addnote() {
+    //assigning values of input to variables
+    const newTitle = document.getElementById('new-title').value
+    const newDescription = document.getElementById('new-Description').value
+    const newDate = document.getElementById('new-Date').value
+
+    if (newTitle && newDescription && newDate){
+        const list = document.createElement("li")
+        list.innerHTML = "<a href = '#'> \
+                        <h2>Title</h2> \
+                        <p>Description</p> \
+                        <p>Date</p>\
+                    </a>"
+        const ticket = document.querySelector(".tickets")
+        ticket.insertBefore(list, ticket.firstElementChild)
+
+        //adding values to elements 
+        const addTitle = document.querySelector(".tickets h2")
+        addTitle.innerHTML = newTitle 
+        const addDescription = document.querySelector('.tickets :nth-child(2)')
+        addDescription.innerHTML = newDescription 
+        const addDate = document.querySelector('.tickets :nth-child(3)')
+        addDate.innerHTML = newDate 
+    }
 }
+
+const add = document.querySelector(".ticket-button")
+// only when all three are non empty 
+add.addEventListener("click", addnote)        
+
