0c51dc7436a4cda82ee7d49ea1f9607371370d9b
Vikas18597 <khativikas97@gmail.com>
2023-04-06 23:56:27 +0530

    makingnew element add function

--- dist/bundle.js
+++ dist/bundle.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/main.css */ \"./src/styles/main.css\");\n\n\n\n\n//# sourceURL=webpack://todo_list/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/main.css */ \"./src/styles/main.css\");\n\n\nconst addnote = () => {\n    const list = document.createElement(\"li\")\n    \n    document.querySelector(\".tickets\").appendChild(list);\n    const link = document.createElement(\"a\")\n    list.appendChild(link)\n}\n\n//# sourceURL=webpack://todo_list/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './styles/main.css'
 
+const addnote = () => {
+    const list = document.createElement("li")
+    
+    document.querySelector(".tickets").appendChild(list);
+    const link = document.createElement("a")
+    list.appendChild(link)
+}
